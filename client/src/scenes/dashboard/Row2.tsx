925f6a1916442ef42ae3424166c62c8406c0568e
echoCodeScript <rakeshtitty@gmail.com>
2024-02-18 20:31:13 +0530

    Added Pie Chart Visual

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -1,30 +1,48 @@
-import React, { useMemo } from 'react'
-import DashboardBox from '@/components/DashboardBox'
-import { useGetKpisQuery, useGetProductsQuery } from '@/state/api'
-import BoxHeader from '@/components/BoxHeader';
-import { Line,LineChart,XAxis,YAxis,ResponsiveContainer,CartesianGrid,Tooltip,Legend } from 'recharts';
-import { useTheme } from '@mui/material';
-const Row2=(()=> {
+import React, { useMemo } from "react";
+import DashboardBox from "@/components/DashboardBox";
+import { useGetKpisQuery, useGetProductsQuery } from "@/state/api";
+import BoxHeader from "@/components/BoxHeader";
+import {
+  Line,
+  LineChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+import { Box, Typography, useTheme } from "@mui/material";
+import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
+import FlexBetween from "@/components/FlexBetween";
+const pieData = [
+  { name: "Group A", value: 600 },
+  { name: "Group B", value: 400 },
+];
+const Row2 = () => {
   const { data: operationalData } = useGetKpisQuery();
   const { data: productData } = useGetProductsQuery();
   const { palette } = useTheme();
+  const pieColors = [palette.primary[800], palette.primary[300]];
   // console.log("🚀 ~ Row2 ~ data :", data)
   const operationalExpenses = useMemo(() => {
     return (
       operationalData &&
-      operationalData[0].monthlyData.map(({ month, expenses,operationalExpenses}) => {
-        return {
-          name: month.substring(0, 3),
-          "Operational Expenses":operationalExpenses,
-          "Non Operational Expenses":(expenses-operationalExpenses).toFixed(2)
-        };
-      })
+      operationalData[0].monthlyData.map(
+        ({ month, expenses, operationalExpenses }) => {
+          return {
+            name: month.substring(0, 3),
+            "Operational Expenses": operationalExpenses,
+            "Non Operational Expenses": (
+              expenses - operationalExpenses
+            ).toFixed(2),
+          };
+        }
+      )
     );
   }, [operationalData]);
-  console.log(operationalData)
+  console.log(operationalData);
   return (
-      <>
-         <DashboardBox gridArea="d">
+    <>
+      <DashboardBox gridArea="d">
         <BoxHeader
           title="Operational vs Non-Operational Expenses"
           subtitle="Top line: Revenue, Bottom line: Expenses"
@@ -76,11 +94,54 @@ const Row2=(()=> {
           </LineChart>
         </ResponsiveContainer>
       </DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
+      <DashboardBox gridArea="e">
+        <BoxHeader title="Campaign and Targets" sideText="+4%" />
+        <FlexBetween mt="0.25rem" gap="1.5rem" pr="1rem">
+          <PieChart
+            width={110}
+            height={100}
+            margin={{
+              top: 0,
+              right: -10,
+              left: 10,
+              bottom: 0,
+            }}
+          >
+            <Pie
+              stroke="none"
+              data={pieData}
+              innerRadius={18}
+              outerRadius={38}
+              paddingAngle={2}
+              dataKey="value"
+            >
+              {pieData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={pieColors[index]} />
+              ))}
+            </Pie>
+          </PieChart>
+          <Box ml="-0.7rem" flexBasis="40%" textAlign="center">
+            <Typography variant="h5">Target Sales</Typography>
+            <Typography m="0.3rem 0" variant="h3" color={palette.primary[300]}>
+              83
+            </Typography>
+            <Typography variant="h6">
+            
+              Finance Goals of the campaign that is desired
+            </Typography>
+          </Box>
+          <Box flexBasis="40%" textAlign="center">
+            <Typography variant="h5">Losses in Revenue</Typography>
+            <Typography variant="h6">Losses are down 25%</Typography>
+            <Typography mt="0.4rem" variant="h5">
+              Margins are up by 30% from last month
+            </Typography>
+          </Box>
+        </FlexBetween>
+      </DashboardBox>
       <DashboardBox gridArea="f"></DashboardBox>
-          
-      </>
-  )
-})
+    </>
+  );
+};
 
-export default Row2
+export default Row2;
